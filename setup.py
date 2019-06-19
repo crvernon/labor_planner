3d4465afbb9d0ba5980177ce8bc99ba63b81fec3
crvernon <chrisrvernon@gmail.com>
2019-06-19 13:05:02 -0600

    packaging

---
+++ setup.py
@@ -0,0 +1,41 @@
+"""setup.py
+
+Module wrapper for `labor_planner`
+
+@author: Chris R. Vernon
+
+"""
+
+
+class VersionError(Exception):
+    def __init__(self, *args, **kwargs):
+        Exception.__init__(self, *args, **kwargs)
+
+
+try:
+    from setuptools import setup, find_packages
+except ImportError:
+    print("Must have setuptools installed to run setup.py. Please install and try again.")
+    raise
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+def get_requirements():
+    with open('requirements.txt') as f:
+        return f.read().split()
+
+setup(
+    name='labor_planner',
+    version='0.1.0',
+    packages=find_packages(),
+    url='https://github.com/crvernon/labor_planner',
+    license='MIT',
+    author='Chris R. Vernon',
+    author_email='chris.vernon@pnnl.gov',
+    description='Plan and visualize staff labor fiscal year allocation',
+    python_requires='>=3.3.*, <4'
+)
